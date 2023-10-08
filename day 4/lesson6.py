e49a8efd9340b0b01c9ede9bbd565a08ef7d588b
erekle shervashidze <shervashidzeerekle@gmail.com>
2023-10-08 12:02:50 +0400

    first commit

---
+++ day 4/lesson6.py
@@ -0,0 +1,8 @@
+user_salary = int(input("anter any_salaryuser_salaryber: "))
+
+if user_salary >=10000:
+    print("გოა-ში სწავლობდი?")
+elif user_salary>1000 and user_salary<10000:
+    print("you mid")
+elif user_salary<=1000:
+    print("შემოსულიყავი გოაში, მატრიცელო")
