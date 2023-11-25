7de50847af27159ca4ef63277c71601131e1c97d
erekle shervashidze <shervashidzeerekle@gmail.com>
2023-11-26 01:02:11 +0400

    first commit

--- day2/lesson3.py
+++ day2/lesson3.py
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 father = 37
 mother = 33
 me = 15
 sister = 12
 
 print(father + mother + me + sister)
-
-=======
 father = 37
 mother = 33
 me = 15
 sister = 12
 
 print(father + mother + me + sister)
-
->>>>>>> 9c3c85066d3b5bf6c2c1a03ee2ea76eec309be03
 print(me + 10 + father)
